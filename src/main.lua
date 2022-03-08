26ddb5d0be4b19fc3e3a4dbc76344bfea7a09cfd
rhinorunner <cubezaine@gmail.com>
2022-03-08 16:04:09 -0500

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -1,177 +1,89 @@
-local gameInfo = {
-    ["Name"] = "Chess",
-    ["Version"] = "0.0.1",
-    ["Creators"] = {"Bertie","Zaine"}
+defaultSetup = {
+	[1] = {
+		["a"] = "white rook",
+		["b"] = "white knight",
+		["c"] = "white bishop",
+		["d"] = "white queen",
+		["e"] = "white king",
+		["f"] = "white bishop",
+		["g"] = "white knight",
+		["h"] = "white rook",
+	},
+	[2] = {
+		["a"] = "white pawn",
+		["b"] = "white pawn",
+		["c"] = "white pawn",
+		["d"] = "white pawn",
+		["e"] = "white pawn",
+		["f"] = "white pawn",
+		["g"] = "white pawn",
+		["h"] = "white pawn",
+	},
+	[3] = {
+		["a"] = " ",
+		["b"] = " ",
+		["c"] = " ",
+		["d"] = " ",
+		["e"] = " ",
+		["f"] = " ",
+		["g"] = " ",
+		["h"] = " ",
+	},
+	[4] = {
+		["a"] = " ",
+		["b"] = " ",
+		["c"] = " ",
+		["d"] = " ",
+		["e"] = " ",
+		["f"] = " ",
+		["g"] = " ",
+		["h"] = " ",
+	},
+	[5] = {
+		["a"] = " ",
+		["b"] = " ",
+		["c"] = " ",
+		["d"] = " ",
+		["e"] = " ",
+		["f"] = " ",
+		["g"] = " ",
+		["h"] = " ",
+	},
+	[6] = {
+		["a"] = " ",
+		["b"] = " ",
+		["c"] = " ",
+		["d"] = " ",
+		["e"] = " ",
+		["f"] = " ",
+		["g"] = " ",
+		["h"] = " ",
+	},
+	[7] = {
+		["a"] = "black pawn",
+		["b"] = "black pawn",
+		["c"] = "black pawn",
+		["d"] = "black pawn",
+		["e"] = "black pawn",
+		["f"] = "black pawn",
+		["g"] = "black pawn",
+		["h"] = "black pawn",
+	},
+	[8] = {
+		["a"] = "black rook",
+		["b"] = "black knight",
+		["c"] = "black bishop",
+		["d"] = "black queen",
+		["e"] = "black king",
+		["f"] = "black bishop",
+		["g"] = "black knight",
+		["h"] = "black rook",
+	},
 }
 
-local defaultSetup = {
-    [1] = {
-        ["a"] = "black rook",
-        ["b"] = "black knight",
-        ["c"] = "black bishop",
-        ["d"] = "black queen",
-        ["e"] = "black king",
-        ["f"] = "black bishop",
-        ["g"] = "black knight",
-        ["h"] = "black rook",
-    },
-    [2] = {
-        ["a"] = " ",
-        ["b"] = " ",
-        ["c"] = " ",
-        ["d"] = " ",
-        ["e"] = " ",
-        ["f"] = " ",
-        ["g"] = " ",
-        ["h"] = " ",
-    },
-    [3] = {
-        ["a"] = " ",
-        ["b"] = " ",
-        ["c"] = " ",
-        ["d"] = " ",
-        ["e"] = " ",
-        ["f"] = " ",
-        ["g"] = " ",
-        ["h"] = " ",
-    },
-    [4] = {
-        ["a"] = " ",
-        ["b"] = " ",
-        ["c"] = " ",
-        ["d"] = " ",
-        ["e"] = " ",
-        ["f"] = " ",
-        ["g"] = " ",
-        ["h"] = " ",
-    },
-    [5] = {
-        ["a"] = " ",
-        ["b"] = " ",
-        ["c"] = " ",
-        ["d"] = " ",
-        ["e"] = " ",
-        ["f"] = " ",
-        ["g"] = " ",
-        ["h"] = " ",
-    },
-    [6] = {
-        ["a"] = " ",
-        ["b"] = " ",
-        ["c"] = " ",
-        ["d"] = " ",
-        ["e"] = " ",
-        ["f"] = " ",
-        ["g"] = " ",
-        ["h"] = " ",
-    },
-    [7] = {
-        ["a"] = " ",
-        ["b"] = " ",
-        ["c"] = " ",
-        ["d"] = " ",
-        ["e"] = " ",
-        ["f"] = " ",
-        ["g"] = " ",
-        ["h"] = " ",
-    },
-    [8] = {
-        ["a"] = "white rook",
-        ["b"] = "white knight",
-        ["c"] = "white bishop",
-        ["d"] = "white queen",
-        ["e"] = "white king",
-        ["f"] = "white bishop",
-        ["g"] = "white knight",
-        ["h"] = "white rook",
-    },
-}
-
-local funkyTestSetup = {
-    [1] = {
-        ["a"] = "black knight",
-        ["b"] = "black bishop",
-        ["c"] = "black rook",
-        ["d"] = "black queen",
-        ["e"] = "black king",
-        ["f"] = "black rook",
-        ["g"] = "black bishop",
-        ["h"] = "black knight",
-    },
-    [2] = {
-        ["a"] = " ",
-        ["b"] = " ",
-        ["c"] = " ",
-        ["d"] = " ",
-        ["e"] = " ",
-        ["f"] = " ",
-        ["g"] = " ",
-        ["h"] = " ",
-    },
-    [3] = {
-        ["a"] = " ",
-        ["b"] = " ",
-        ["c"] = " ",
-        ["d"] = " ",
-        ["e"] = " ",
-        ["f"] = " ",
-        ["g"] = " ",
-        ["h"] = " ",
-    },
-    [4] = {
-        ["a"] = " ",
-        ["b"] = " ",
-        ["c"] = " ",
-        ["d"] = " ",
-        ["e"] = " ",
-        ["f"] = " ",
-        ["g"] = " ",
-        ["h"] = " ",
-    },
-    [5] = {
-        ["a"] = " ",
-        ["b"] = " ",
-        ["c"] = " ",
-        ["d"] = " ",
-        ["e"] = " ",
-        ["f"] = " ",
-        ["g"] = " ",
-        ["h"] = " ",
-    },
-    [6] = {
-        ["a"] = " ",
-        ["b"] = " ",
-        ["c"] = " ",
-        ["d"] = " ",
-        ["e"] = " ",
-        ["f"] = " ",
-        ["g"] = " ",
-        ["h"] = " ",
-    },
-    [7] = {
-        ["a"] = " ",
-        ["b"] = " ",
-        ["c"] = " ",
-        ["d"] = " ",
-        ["e"] = " ",
-        ["f"] = " ",
-        ["g"] = " ",
-        ["h"] = " ",
-    },
-    [8] = {
-        ["a"] = "white knight",
-        ["b"] = "white bishop",
-        ["c"] = "white rook",
-        ["d"] = "white queen",
-        ["e"] = "white king",
-        ["f"] = "white rook",
-        ["g"] = "white bishop",
-        ["h"] = "white knight",
-    },
-}
 currentBoard = defaultSetup
 
-function convert(str) do
+function convert(str)
 	local converted = {}
 	for i in string.gmatch(str,"%a+") do
 		table.insert(converted,i)
@@ -181,26 +93,43 @@ function convert(str) do
 		reStr = reStr .. string.sub(i,1,1)
 	end
 	return reStr
-	end
 end
 
-function showBoard() do
+function showBoard()
 	letters = {"a","b","c","d","e","f","g","h"}
-	for num=1, 8 do
-		if currentBoard[num]["a"] then
-			if (num%2 == 0) then
-				io.write(" "..currentBoard[num]["a"].." ")
+	for v,x in pairs(letters) do
+		for i=1, 8 do
+			if v%2 == 0 then
+				if currentBoard[i][x] ~= " " and currentBoard[i][x] ~= nil then
+					if i%2 == 0 then
+						io.write("█"..convert(currentBoard[i][x]).."█")
+					else 
+						io.write(" "..convert(currentBoard[i][x]).." ")
+					end
+				else 
+					if i%2 == 0 then
+						io.write("████")
+					else 
+						io.write("    ")
+					end
+				end
 			else 
-				io.write("█"..currentBoard[num]["a"].."█")
-			end
-		else 
-			if num%2 == 0 then
-				io.write("    ")
-			else
-				io.write("████")
+				if currentBoard[i][x] ~= " " and currentBoard[i][x] ~= nil then
+					if i%2 == 0 then
+						io.write(" "..convert(currentBoard[i][x]).." ")
+				else 
+						io.write("█"..convert(currentBoard[i][x]).."█")
+					end
+				else 
+					if i%2 == 0 then
+						io.write("    ")
+					else 
+						io.write("████")
+					end
+				end
 			end
 		end
+		print()
 	end
-end
 end
 showBoard()
